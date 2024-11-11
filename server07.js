c2197d2153adcf1a31e212c2f4bd57f8240bfa00
Binnur KURT <deepcloudlabs@gmail.com>
2024-11-11 20:16:21 +0300

    Update server07.js

--- server07.js
+++ server07.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 let symbols = await fetch("https://api.binance.com/api/v3/ticker/price")
     .then(res => res.json())
     .then(tickers => tickers.map(ticker => ticker.symbol))
-let promises = symbols.map( symbol => fetch(`https://api.binance.com/api/v3/ticker?symbol=${symbol}`))
+let promises = symbols.splice(100).map( symbol => fetch(`https://api.binance.com/api/v3/ticker?symbol=${symbol}`))
 Promise.all(promises)
         .then( responses => {
             responses.forEach(response => response.json().then(console.log))
